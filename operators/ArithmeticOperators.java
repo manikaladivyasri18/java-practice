4a54d9e15d15ccbb1c79cc622d520e34bd369a44
Your Name <manikaladivyasri18@gmail.com>
2026-07-07 21:39:18 +0530

    Organized java files into Basic folder

---
+++ operators/ArithmeticOperators.java
@@ -0,0 +1,13 @@
+public class ArithmeticOperators
+{
+    public static void main(String a[])
+    {
+        
+        int num1=4;
+        int num2=10;
+        int result;
+        result=num1+num2;
+        System.out.println(result);
+    }
+
+}
---
+++ operators/logicalOperators.java
@@ -0,0 +1,15 @@
+public  class logicalOperators
+{
+    public static void main(String a[])
+    {
+        int x=1;
+        int y=2;
+        if(x>0&&y>0)
+        {
+            System.out.println("TRUE");
+        }
+        else{
+            System.out.println("FALSE");
+        }
+    }
+}
