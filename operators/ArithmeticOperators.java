f56a631438e8a1dbe36bd4226d1f661545c6e5e4
Your Name <manikaladivyasri18@gmail.com>
2026-07-07 21:52:33 +0530

    Update arithmetic operators

--- operators/ArithmeticOperators.java
+++ operators/ArithmeticOperators.java
@@ -5,9 +5,18 @@ public class ArithmeticOperators
         
         int num1=4;
         int num2=10;
-        int result;
-        result=num1+num2;
-        System.out.println(result);
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1%num2);
+        System.out.println(num1/num2);
+        System.out.println(num1*num2);
+
+
+
+
+
+
+
     }
 
 }
