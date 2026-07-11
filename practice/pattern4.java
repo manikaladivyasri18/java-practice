68aafd86a62cd4be2150ba2836940ed42cbbde55
Your Name <manikaladivyasri18@gmail.com>
2026-07-11 20:08:22 +0530

    updated pattern problems

---
+++ practice/pattern4.java
@@ -0,0 +1,15 @@
+public class pattern4
+{
+    public static void main(String a[])
+    {
+        int n=5;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
