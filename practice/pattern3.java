92e5f768df08c12bfdfe582a0ba6aa1aeb42a295
Your Name <manikaladivyasri18@gmail.com>
2026-07-11 18:36:34 +0530

    updated pattern program

---
+++ practice/pattern3.java
@@ -0,0 +1,16 @@
+public class pattern3
+{
+    public static void main(String a[])
+    {
+        int n=5;
+        for(int i=5;i>0;i--)
+        {
+            for(int j=i;j>0;j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
