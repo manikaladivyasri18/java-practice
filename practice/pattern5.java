e5d6612a90b5c7181ff8ab57e811031f293e703b
Your Name <manikaladivyasri18@gmail.com>
2026-07-12 10:04:15 +0530

    added pattern problems

---
+++ practice/pattern5.java
@@ -0,0 +1,16 @@
+public class pattern5
+{
+    public static void main(String a[])
+    {
+        int n=5;
+        for(int i=5;i>0;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+
+            }
+            System.out.println();
+        }
+    }
+}
