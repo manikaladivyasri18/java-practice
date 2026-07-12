8233a4b24a639700909cbbc324ca2b7dc5f0d9f8
Your Name <manikaladivyasri18@gmail.com>
2026-07-12 18:42:21 +0530

    Added pattern programs

---
+++ practice/pyramid.java
@@ -0,0 +1,26 @@
+public class pyramid
+{
+    public static void main (String a[])
+    {
+        int n=5;
+        for (int i=1;i<=n;i++)
+        {
+            for(int s=n-i;s>0;s--)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<i+1;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+            
+        
+    
+            
+        
+            
+        
+    }
+}
