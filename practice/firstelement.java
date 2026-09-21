000ee879e87e6c5ca004b54ce82a1ec8c049688f
Your Name <manikaladivyasri18@gmail.com>
2026-09-21 21:33:11 +0530

    practiced hashmap concept

---
+++ practice/firstelement.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class firstelement
+{
+    public static void main(String a[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:arr)
+        {
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int i:arr)
+        {
+            if(map.get(i)==1)
+            {
+                System.out.println(i);
+                break;
+            }
+
+        }
+    }
+}
