846facdc1abc6ed60826d3466be01b66ebbdfe2e
Your Name <manikaladivyasri18@gmail.com>
2026-09-22 18:54:55 +0530

    learned HashMap concepts

---
+++ practice/Frequencyofnum.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class Frequencyofnum
+{
+    public static void main(String a[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i:arr)
+        {
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int i:arr)
+        {
+            if(map.get(i)==2)
+            {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
